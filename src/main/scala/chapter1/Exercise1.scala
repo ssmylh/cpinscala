67b4d73d67f892ab7f7e01f8a7d896ba3190c784
ssmylh <ssmylh@gmail.com>
2015-10-10 23:15:36 +0900

    Chapter 1 Exercise

---
+++ src/main/scala/chapter1/Exercise1.scala
@@ -0,0 +1,30 @@
+package chapter1
+
+object Exercise1 {
+  // 1
+  def compose[A, B, C](g: B => C, f: A => B): A => C = a => g(f(a))
+
+  // 2
+  def fuse[A, B](oa: Option[A], ob: Option[B]): Option[(A, B)] = for {
+    a <- oa
+    b <- ob
+  } yield (a, b)
+
+  // 3
+  def check[T](xs: Seq[T])(pred: T => Boolean): Boolean = xs.forall { x =>
+    try {
+      pred(x)
+    } catch {
+      case _: Exception => false
+    }
+  }
+
+  // 4
+  def permutations(s: String): Seq[String] = {
+    if (s.length == 0) Seq("")
+    else for {
+      i <- 0 until s.length
+      x <- permutations(s.take(i) + s.takeRight(s.length - i - 1))
+    } yield s(i) + x
+  }
+}
