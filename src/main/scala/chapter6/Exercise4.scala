2ca8bf4cdabe042e0dca9aab770c800041eb576b
ssmylh <ssmylh@gmail.com>
2016-03-14 20:33:34 +0900

    Pass the latest event correctly in Chapter 6 Exercise 4
    About zip and scan method, pass the latest event to new `Signal` instance.

--- src/main/scala/chapter6/Exercise4.scala
+++ src/main/scala/chapter6/Exercise4.scala
@@ -29,11 +29,18 @@ class Signal[T] {
     case _ => new Signal(observable.map(f))
   }
 
-  def zip[S](that: Signal[S]): Signal[(T, S)] =
-    new Signal(observable.zip(that.observable))
+  def zip[S](that: Signal[S]): Signal[(T, S)] = (lastEvent, that.lastEvent) match {
+    case (Some(t), Some(s)) => new Signal(observable.zip(that.observable), (t, s))
+    case (_, _) => new Signal(observable.zip(that.observable))
+  }
 
-  def scan[S](z: S)(f: (S, T) => S): Signal[S] =
-    new Signal(observable.scan(z)(f))
+  def scan[S](z: S)(f: (S, T) => S): Signal[S] = lastEvent match {
+    case Some(t) => {
+      val s = f(z, t)
+      new Signal(observable.scan(s)(f), s)
+    }
+    case _ => new Signal(observable.scan(z)(f))
+  }
 }
 
 object Exercise4 extends App {
@@ -56,9 +63,11 @@ object Exercise4 extends App {
 
   val o31 = Subject[Int]()
   val o32 = Subject[String]()
-  val zipped = o31.toSignal.zip(o32.toSignal)
+  val s31 = o31.toSignal
+  val s32 = o32.toSignal
   o31.onNext(1)
   o32.onNext("a")
+  val zipped = s31.zip(s32)
   assert(zipped() == (1, "a"))
   o31.onNext(2)
   o32.onNext("b")
@@ -66,8 +75,8 @@ object Exercise4 extends App {
 
   val o4 = Subject[Int]()
   val s4 = o4.toSignal
-  val sum = s4.scan(0)(_ + _)
   o4.onNext(1)
+  val sum = s4.scan(0)(_ + _)
   o4.onNext(2)
   assert(sum() == 3)
   o4.onNext(3)
