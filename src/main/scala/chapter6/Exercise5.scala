80d83741af01852945b12ed2fbaade917e4e536f
ssmylh <ssmylh@gmail.com>
2016-03-16 20:37:24 +0900

    Fix test in Chapter 6 Exercise 5

--- src/main/scala/chapter6/Exercise5.scala
+++ src/main/scala/chapter6/Exercise5.scala
@@ -12,20 +12,33 @@ class RCell[T] extends Signal[T] {
 }
 
 object Exercise5 extends App {
-  val elapsedSeconds = new RCell[Int]
-  val elapsedMinutes = elapsedSeconds.map(_ / 60)
-  val elaplsedHours = elapsedMinutes.map(_ / 60)
+  val rc1 = new RCell[Int]()
+  rc1 := 1
+  assert(rc1() == 1)
 
-  val sec1 = 1 * 60 * 60 + 2 * 60;
-  elapsedSeconds := sec1
-  assert(elapsedSeconds() == sec1)
-  assert(elapsedMinutes() == 60 + 2)
-  assert(elaplsedHours() == 1)
+  val rc2 = new RCell[Int]()
+  rc2 := 1
+  val increment = rc2.map(_ + 1)
+  assert(increment() == 2)
+  rc2 := 2
+  assert(increment() == 3)
 
-  val sec2 = 3 * 60 * 60 + 4 * 60
-  elapsedSeconds := sec2
-  assert(elapsedSeconds() == sec2)
-  assert(elapsedMinutes() == 3 * 60 + 4)
-  assert(elaplsedHours() == 3)
+  val rc31 = new RCell[Int]()
+  val rc32 = new RCell[String]()
+  rc31 := 1
+  rc32 := "a"
+  val zipped = rc31.zip(rc32)
+  assert(zipped() == (1, "a"))
+  rc31 := 2
+  rc32 := "b"
+  assert(zipped() == (2, "b"))
 
+  val rc4 = new RCell[Int]()
+  rc4 := 1
+  val sum = rc4.scan(10)(_ + _)
+  assert(sum() == 10)
+  rc4 := 2
+  assert(sum() == 12)
+  rc4 := 3
+  assert(sum() == 15)
 }
