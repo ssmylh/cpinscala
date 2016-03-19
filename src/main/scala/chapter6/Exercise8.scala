a64e78228f575295725d4f97fac73d4dc9562f71
ssmylh <ssmylh@gmail.com>
2016-03-19 19:38:56 +0900

    Chapter 6 Exercise 7

--- src/main/scala/chapter6/Exercise8.scala
+++ src/main/scala/chapter6/Exercise8.scala
@@ -10,70 +10,59 @@ import rx.lang.scala._
 
 object Exercise8 extends App {
 
-  def log(msg: String): Unit = println(s"${Thread.currentThread.getName} - $msg")
-
-  val copy = Observable.create[Long] { obs =>
-    obs.onNext(0)
-    for (i <- 1 to 10) {
-      Thread.sleep(500)
-//      if (i == 6) {
-//        obs.onError(new Exception("test"))
-//      }
-      obs.onNext(i)
+  def copyFile(src: String, dest: String): Observable[Double] = {
+    val dummy = Observable.create[Double] { obs =>
+      obs.onNext(0)
+      for (i <- 1 to 100) {
+        Thread.sleep(50)
+        obs.onNext(i)
+      }
+      obs.onNext(101d)
+      obs.onCompleted()
+      Subscription()
     }
-    obs.onCompleted()
-    Subscription()
+
+    def copy(src: String, dest: String): Observable[Double] = Observable.create { obs =>
+      obs.onNext(0d)
+
+      val file = new File(src)
+      val size = file.length()
+      val in = new BufferedInputStream(new FileInputStream(file))
+      val out = new BufferedOutputStream(new FileOutputStream(dest))
+      try {
+        var copiedSize: Long = 0l
+        val buffer = new Array[Byte](1024)
+        @annotation.tailrec
+        def go(): Unit = {
+          val readBytes = in.read(buffer)
+          if (readBytes != -1) {
+            out.write(buffer, 0, readBytes)
+            copiedSize += readBytes
+            if (copiedSize == size) {
+              obs.onNext(100d)
+            } else {
+              obs.onNext(copiedSize.toDouble / size.toDouble)
+            }
+            go()
+          }
+        }
+
+        go()
+        obs.onNext(101d)
+        obs.onCompleted()
+      } catch {
+        case e: Exception => obs.onError(e)
+      } finally {
+        in.close()
+        out.close()
+      }
+      Subscription()
+    }
+
+    Observable.interval(0.1.seconds).combineLatest(dummy).takeWhile(_._2 <= 100).map(_._2)
   }
 
-  //val o = copy.subscribeOn(schedulers.ComputationScheduler())
-  //val o = copy.subscribeOn(schedulers.ComputationScheduler())
-  val interval = Observable.interval(0.1.seconds)
-
-  interval.combineLatest(copy).takeWhile(x => x._2 < 10).subscribe(x => log(s"$x"), e => e.printStackTrace())
-
-//  val sig = new Signal(copy.observeOn(schedulers.ComputationScheduler()))
-//  println(1)
-//  println(sig())
-
+  def log(msg: String): Unit = println(s"${Thread.currentThread.getName} - $msg")
+  val o = copyFile("", "").subscribe(x => log(s"$x %"))
   Thread.sleep(10 * 1000)
-
-  //  def copyFile(src: String, dest: String): Observable[Double] = {
-  //
-  //    def withProgress(src: String, dest: String): Observable[Double] = Observable.create { obs =>
-  //      val file = new File(src)
-  //      val size = file.length()
-  //
-  //      val in = new BufferedInputStream(new FileInputStream(file))
-  //      val out = new BufferedOutputStream(new FileOutputStream(dest))
-  //      try {
-  //        var progressSize = 0
-  //
-  //        val buffer = new Array[Byte](8192)
-  //        def go(): Unit = {
-  //          val readBytes = in.read(buffer)
-  //          if (readBytes != -1) {
-  //            out.write(buffer, 0, readBytes)
-  //            progressSize += readBytes
-  //            obs.onNext(progressSize.toDouble / size.toDouble)
-  //            go()
-  //          }
-  //        }
-  //        go()
-  //        obs.onCompleted()
-  //      } catch {
-  //        case e: IOException => obs.onError(e)
-  //      } finally {
-  //        in.close()
-  //        out.close()
-  //      }
-  //
-  //      Subscription()
-  //    }
-  //
-  //    for {
-  //      _ <- Observable.interval(0.1.seconds)
-  //      p <- withProgress(src, dest)
-  //    } yield p
-  //  }
-
 }
